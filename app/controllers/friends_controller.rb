a966f288859b8a1b0ac0ee6f66df2e1324a3d39e
Braydon Wang <b392wang@uwaterloo.ca>
2023-06-06 23:13:26 -0400

    adding controllers

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,5 +1,6 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /friends or /friends.json
   def index
@@ -57,6 +58,10 @@ class FriendsController < ApplicationController
     end
   end
 
+  def correct_user
+    @friend = current_user.friends.find_by(id: params[:id])
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_friend
@@ -65,6 +70,6 @@ class FriendsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def friend_params
-      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :twitter)
+      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :twitter, :user_id)
     end
 end
